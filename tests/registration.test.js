42d1298391ed2cdbe8e4eb00d98939a09cb0912f
sibusisozibeko <sibusisozibeko@gmail.com>
2018-07-11 11:15:27 +0200

    added an alert

--- tests/registration.test.js
+++
@@ -1,9 +0,0 @@
-describe('The RegiStration function', function(){
-
-    it('must show registration numbers from Cape Town ', function(){
-      var Reg = RegiStration({"CA 3443":0,"CA 87643":0,"CA 3973":0});
-
-      assert.deepEqual({"CA 3443": 0 ,"CA 87643":0 , "CA 3973":0}, Reg.regMap());
-
-    });
-});
